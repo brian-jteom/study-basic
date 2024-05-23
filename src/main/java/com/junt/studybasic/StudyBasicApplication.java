05c80cd8d6bbe683a641dec248ae0e221643433f
Eom Juntae <junbrian9815@gmail.com>
2024-05-23 10:47:59 +0900

    initail commit

---
+++ src/main/java/com/junt/studybasic/StudyBasicApplication.java
@@ -0,0 +1,13 @@
+package com.junt.studybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junt/studybasic/StudyBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junt.studybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
